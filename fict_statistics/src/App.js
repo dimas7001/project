90bacc0a8e62ce55c83fac04b6336ca2df67f886
dimas7001 <54948234+dimas7001@users.noreply.github.com>
2020-02-27 22:35:12 +0200

    iaiaiaiaiaiaiaiaiaiaiaiaiaiaiaia

--- fict_statistics/src/App.js
+++ fict_statistics/src/App.js
@@ -28,83 +28,80 @@ import {
 export default class App extends React.Component {
   render() {
     const mydata = {
-  data: [{
-    neatExplosions: 7,
-    wow: 10,
-    dog: 8,
-    sickMoves: 9,
-    nice: 7
-  }],
-  domains: [
-    {name: 'nice', domain: [0, 100]},
-    {name: 'explosions', domain: [6.9, 7.1], getValue: d => d.neatExplosions},
-    {name: 'wow', domain: [0, 11]},
-    {name: 'sickMoves', domain: [0, 20]}
-  ],
-  height: 300,
-  width: 400
-};
+      data: [{
+        neatExplosions: 7,
+        wow: 10,
+        dog: 8,
+        sickMoves: 9,
+        nice: 7
+      }],
+      domains: [
+        {name: 'nice', domain: [0, 100]},
+        {name: 'explosions', domain: [6.9, 7.1], getValue: d => d.neatExplosions},
+        {name: 'wow', domain: [0, 11]},
+        {name: 'sickMoves', domain: [0, 20]}
+      ],
+      height: 300,
+      width: 400
+      }; 
+
     return (
       <div className="App">
-        <header className="App-header">
-          <ul class="navigation">
-            <li class="item"><div>item1</div></li>
-            <li class="item"><div>item2</div></li>
-            <li class="item"><div>item3</div></li>
-            <li class="item"><div>item4</div></li>
-          </ul> 
-        </header>
-        <div>
-          <XYPlot
-            className="barSeries"
-            xType="ordinal"
-            stackBy="y"
-            width={600}
-            height={400}
-          >
-            <XAxis />
-            <YAxis />
-            <VerticalBarSeries
-              barWidth="0.5"
-              color="#a4b9aa"
-              stroke="grey"
-              data={[
-                {x: 'Z', y: 10},
-                {x: 'A', y: 5},
-                {x: 'L', y: 15},
-                {x: 'U', y: 20},
-                {x: 'P', y: 15},
-                {x: 'a', y: 20}
-              ]}
-            />
-          </XYPlot> 
-          <XYPlot
-            className="barSeries"
-            xType="ordinal"
-            stackBy="y"
-            width={600}
-            height={400}
-          >
-          <HorizontalGridLines/>
-          <VerticalGridLines/>
-          <XAxis />
-          <YAxis />
-          <RadarChart data={mydata} style={{
-            axes: {
-              line: {},
-              ticks: {},
-              text: {}
-            },
-            labels: {
-              fontSize: 10
-            },
-            polygons: {
-              strokeWidth: 0.5,
-              strokeOpacity: 1,
-              fillOpacity: 0.1
-            }
-          }}/>     
-          </XYPlot> 
+        <div className="info">
+          <div className="photo">
+            <div>
+              <img src="photos/default.png" alt="photo"/>
+            </div>
+            <p className="middleBold">зав. каф. Павлов О. А.</p>
+          </div>
+          <div className="statistics">
+            <div>
+              <h2>83.3%</h2>
+              <p className="normal">опитаних вважають, що <br/>
+                КПІ варто продовжити <br/>
+                контракт з цим <br/>
+                викладачем</p>
+            </div>
+            <div>
+              <h2>62.7%</h2>
+              <p className="normal">гарно чують викладача</p>
+            </div>
+            <div>
+              <h2>13</h2>
+              <p className="normal">кількість опитаних</p>
+            </div>
+          </div>
+        </div>
+        <div className="graphics">
+          <div className="radar">
+
+          </div>
+          <div className="bar">
+            <div>
+              <XYPlot
+                className="barSeries"
+                xType="ordinal"
+                stackBy="y"
+                width={415}
+                height={415}>
+                <YAxis/>
+                <VerticalBarSeries
+                  barWidth="0.85"
+                  color="#ffffff"
+                  data={[
+                    {x: '1', y: 10},
+                    {x: '2', y: 30},
+                    {x: '3', y: 15},
+                    {x: '4', y: 45},
+                    {x: '5', y: 90}
+                  ]}
+                />
+              </XYPlot>
+            </div>
+            <div>
+
+            </div>
+          </div>
         </div>
       </div>
     );
